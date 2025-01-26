c80d64cf2370754c5176c75be4f7c390394699bc
CodingWinner <hellosapna1982@gmail.com>
2025-01-26 11:47:42 -0500

    Fixed inclusion of the header and opencl file.

--- clRL/clRL.cpp
+++ clRL/clRL.cpp
@@ -1,7 +1,7 @@
 ﻿// clRL.cpp : Defines the entry point for the application.
 //
 
-#include "clRL.h"
+#include "include/clRL.h"
 #include <random>
 #include <fstream>
 
@@ -19,13 +19,13 @@ namespace clRL
 
 	void createKernels()
 	{
-#include "clRL.opencl"
+#include "include/clRL.opencl"
 		cl::Program program(context, source);
 		program.build("-cl-std=CL3.0");
 		program.createKernels(&kernels);
 	}
 
-	Layer::Layer(const size_t& neuron_num, const size_t& input_num, const size_t &batch_size, const unsigned int &seed)
+	Layer::Layer(const size_t &neuron_num, const size_t &input_num, const size_t &batch_size, const unsigned int &seed)
 	{
 		neurons = neuron_num;
 		inputs = input_num;
@@ -39,7 +39,7 @@ namespace clRL
 		queue.enqueueFillBuffer(bias_derivatives, 0.0f, 0, sizeof(float) * neurons);
 		queue.enqueueFillBuffer(weight_derivatives, 0.0f, 0, sizeof(float) * neurons * inputs);
 
-		float* weight_values = new float[neurons * inputs];
+		float *weight_values = new float[neurons * inputs];
 		std::mt19937 gen(seed);
 		std::normal_distribution<float> dist(0.0f, std::sqrt(2.0f / inputs));
 		for (size_t i = 0; i < neurons * inputs; i++)
@@ -50,7 +50,7 @@ namespace clRL
 		delete[] weight_values;
 	}
 
-	Layer::Layer(const Layer& l)
+	Layer::Layer(const Layer &l)
 	{
 		biases = cl::Buffer(l.biases);
 		weights = cl::Buffer(l.weights);
@@ -58,18 +58,17 @@ namespace clRL
 		costs = l.costs;
 	}
 
-	const cl::Buffer& Layer::runLayer(const cl::Buffer& ins, const size_t &batch_size)
+	const cl::Buffer &Layer::runLayer(const cl::Buffer &ins, const size_t &batch_size)
 	{
 		cl_command_queue temp_queue = queue();
 
 		// Calculate dot product between inputs and weights
 		clblast::Gemm(clblast::Layout::kRowMajor, clblast::Transpose::kNo, clblast::Transpose::kNo,
-			batch_size, neurons, inputs, 1.0f,
-			ins(), 0, inputs,
-			weights(), 0, neurons,
-			0.0f, outputs(), 0, batch_size,
-			&temp_queue
-		);
+					  batch_size, neurons, inputs, 1.0f,
+					  ins(), 0, inputs,
+					  weights(), 0, neurons,
+					  0.0f, outputs(), 0, batch_size,
+					  &temp_queue);
 
 		// Add bias terms
 		kernels[OUTPUT_ADD_BIAS_KERNEL].setArg(0, outputs);
@@ -80,26 +79,26 @@ namespace clRL
 
 		return outputs;
 	}
-	
-	void Layer::backProp(const cl::Buffer& ins, cl::Buffer& prev_costs, const size_t& batch_size, const float& a, const float& b)
+
+	void Layer::backProp(const cl::Buffer &ins, cl::Buffer &prev_costs, const size_t &batch_size, const float &a, const float &b)
 	{
 		cl_command_queue temp_queue = queue();
 
 		// Calculate new weight derivatives
 		clblast::Gemm(clblast::Layout::kRowMajor, clblast::Transpose::kYes, clblast::Transpose::kNo,
-			inputs, neurons, batch_size, a,
-			ins(), 0, batch_size,
-			costs(), 0, neurons,
-			b, weight_derivatives(), 0, inputs,
-			&temp_queue);
+					  inputs, neurons, batch_size, a,
+					  ins(), 0, batch_size,
+					  costs(), 0, neurons,
+					  b, weight_derivatives(), 0, inputs,
+					  &temp_queue);
 
 		// Calculate previous layer costs
 		clblast::Gemm(clblast::Layout::kRowMajor, clblast::Transpose::kNo, clblast::Transpose::kYes,
-			batch_size, inputs, neurons, 1.0f,
-			costs(), 0, neurons,
-			weights(), 0, neurons,
-			0.0f, prev_costs(), 0, batch_size,
-			&temp_queue);
+					  batch_size, inputs, neurons, 1.0f,
+					  costs(), 0, neurons,
+					  weights(), 0, neurons,
+					  0.0f, prev_costs(), 0, batch_size,
+					  &temp_queue);
 
 		// Calculate new bias derivatives
 		clblast::Scal(neurons * batch_size, a, costs(), 0, 1, &temp_queue);
@@ -122,17 +121,17 @@ namespace clRL
 		queue.enqueueNDRangeKernel(kernels[SUBTRACT_KERNEL], 0, neurons);
 	}
 
-	void Layer::backProp(const cl::Buffer& ins, const size_t& batch_size, const float& a, const float& b)
+	void Layer::backProp(const cl::Buffer &ins, const size_t &batch_size, const float &a, const float &b)
 	{
 		cl_command_queue temp_queue = queue();
 
 		// Calculate new weight derivatives
 		clblast::Gemm(clblast::Layout::kRowMajor, clblast::Transpose::kYes, clblast::Transpose::kNo,
-			inputs, neurons, batch_size, a,
-			ins(), 0, batch_size,
-			costs(), 0, neurons,
-			b, weight_derivatives(), 0, inputs,
-			&temp_queue);
+					  inputs, neurons, batch_size, a,
+					  ins(), 0, batch_size,
+					  costs(), 0, neurons,
+					  b, weight_derivatives(), 0, inputs,
+					  &temp_queue);
 
 		// Calculate new bias derivatives
 		clblast::Scal(neurons * batch_size, a, costs(), 0, 1, &temp_queue);
@@ -154,8 +153,8 @@ namespace clRL
 		kernels[SUBTRACT_KERNEL].setArg(2, neurons);
 		queue.enqueueNDRangeKernel(kernels[SUBTRACT_KERNEL], 0, neurons);
 	}
-	
-	Model::Model(const std::vector<size_t>& neurons, const size_t& initial_input_num, const size_t& batch_size, const unsigned int& seed)
+
+	Model::Model(const std::vector<size_t> &neurons, const size_t &initial_input_num, const size_t &batch_size, const unsigned int &seed)
 	{
 		layers = std::vector<Layer>(neurons.size());
 		layers[0] = Layer(neurons[0], initial_input_num, batch_size, seed);
@@ -165,7 +164,7 @@ namespace clRL
 		}
 	}
 
-	Model::Model(const Model&m)
+	Model::Model(const Model &m)
 	{
 		for (size_t i = 0; i < m.layers.size(); i++)
 		{
@@ -173,7 +172,7 @@ namespace clRL
 		}
 	}
 
-	void Model::getCosts(clEnvironment::Environment& env, const size_t& batch_size)
+	void Model::getCosts(clEnvironment::Environment &env, const size_t &batch_size)
 	{
 		cl::Buffer temp = layers[0].runLayer(env.states, batch_size);
 		cl::Buffer actions(context, FLAGS, batch_size * sizeof(size_t));
@@ -200,7 +199,7 @@ namespace clRL
 		queue.finish();
 	}
 
-	void Model::train(clEnvironment::Environment& env, const size_t& num_epochs, const size_t& batch_size, const float& a, const float& b)
+	void Model::train(clEnvironment::Environment &env, const size_t &num_epochs, const size_t &batch_size, const float &a, const float &b)
 	{
 		cl::Buffer temp;
 		cl::Buffer prev_states;
@@ -228,22 +227,22 @@ namespace clRL
 			kernels[CALCULATE_LOSS_KERNEL].setArg(1, layers[layers.size() - 1].costs);
 			kernels[CALCULATE_LOSS_KERNEL].setArg(2, num_outputs * batch_size);
 			queue.enqueueNDRangeKernel(kernels[CALCULATE_LOSS_KERNEL], 0, num_outputs * batch_size);
-			
+
 			for (size_t j = layers.size() - 1; j > 0; j--)
 			{
-				layers[j].backProp(layers[j - 1].outputs, layers[j-1].costs, batch_size, a, b);
+				layers[j].backProp(layers[j - 1].outputs, layers[j - 1].costs, batch_size, a, b);
 			}
 			layers[0].backProp(prev_states, batch_size, a, b);
 		}
 	}
 
-	void Model::test(clEnvironment::Environment& env, const size_t& num_epochs, const size_t& batch_size, const std::string &file_name)
+	void Model::test(clEnvironment::Environment &env, const size_t &num_epochs, const size_t &batch_size, const std::string &file_name)
 	{
 		cl::Buffer temp;
 		cl::Buffer actions(context, FLAGS, batch_size * sizeof(size_t));
 		size_t num_outputs = layers[layers.size() - 1].neurons;
 		cl_command_queue temp_queue = queue();
-		float* rewards;
+		float *rewards;
 		std::ofstream file(file_name);
 		for (size_t i = 0; i < num_epochs; i++)
 		{
@@ -252,7 +251,7 @@ namespace clRL
 			{
 				temp = layers[j].runLayer(temp, batch_size);
 			}
-			
+
 			for (size_t j = 0; j < batch_size; j++)
 			{
 				clblast::Max<float>(num_outputs, actions(), j, temp(), j * num_outputs, 1, &temp_queue);
